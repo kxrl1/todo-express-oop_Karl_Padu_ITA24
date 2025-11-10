3c3b317e4a267b9aac8c5eddb6f58a2025348079
karl <karl.padu@voco.ee>
2025-11-10 12:26:52 +0200

    Create new Todo from user data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+import todoRoutes from './routes/todos.js'
 
 const app = express()
+app.use(bodyParser.json())
 
-app.get("/json-test", (reg, res) => {
-    res.send({ message: "json test ok"})
-})
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/todos", todoRoutes)
 
 app.listen(3009, () => {
     console.log('Server is running on port 3009') 
