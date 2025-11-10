801bc14d92022053f45092b30ba4fe164ccc8215
karl <karl.padu@voco.ee>
2025-11-10 12:56:29 +0200

    Update the Todo by id and Delete the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -19,6 +19,34 @@ class todoController {
     getTodos(req, res) {
         res.json({tasks: this.TODOS})
     }
+
+    updateTodo(req, res) {
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+
+        console.log(req.body)
+        console.log(req.params)
+
+        const todoIndex = this.TODOS.findIndex(todo => todo.id === todoId)
+
+        if(todoIndex < 0) {
+            res.json({message: 'Could not find todo with such indxe'   
+            })
+            throw new Error('Could not find todo')
+        }
+
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        res.json({
+            message: 'todo is updated',
+            updatedTodo: this.TODOS[todoIndex]
+        })
+    }
+
+    deleteTodo(req, res) {
+        const todoId = req.params.id
+        this.TODOS = this.TODOS.filter(todo => todo.id !== todoId)
+        res.json({message: 'todo deleted'})
+    }
 }
 
 export const TodoController = new todoController()
